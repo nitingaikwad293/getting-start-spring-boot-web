07e1d05b82fe220e7dea609b4b92855966990d45
ngaikwad3 <ngaikwad3@dxc.com>
2023-01-06 18:30:52 +0530

    Adding Login Screen

--- src/main/java/com/nitin/gettingstartspringbootweb/login/LoginController.java
+++ src/main/java/com/nitin/gettingstartspringbootweb/login/LoginController.java
@@ -1,14 +1,18 @@
 package com.nitin.gettingstartspringbootweb.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class LoginController {
 
+	private Logger logger = LoggerFactory.getLogger(this.getClass());	
 	
 	@RequestMapping("login")
 	public String login() {
+   logger.debug("Inside /login method ");		
 		return "login";
 	}
 	
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -5,6 +5,11 @@ Login Application
 </title>
 </head>
 <body>
-<H2>Login Application !</H2>
+Login Application 
+<form method="post">
+Name : <input type="text" name="name">
+Password : <input type="password" name="password">
+<input type="submit"/>
+</form>
 </body>
 </html>
